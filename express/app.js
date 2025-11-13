86f3a02accf2d110742bb0224ba6213876280b9c
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-11-13 14:43:32 +0530

    how to send files in express js fix dirname not working in express js

--- express/app.js
+++ express/app.js
@@ -15,34 +15,6 @@ app.get("/test", (req, res) => {
     res.send("<h1>Hello About Page!</h1>");
 });
 
-app.get("/contact", (req, res) => {
-    res.send(`<form id="shorten-form" autocomplete="off" method="post">
-        <div>
-          <label for="url">Enter URL</label>
-          <input
-            type="url"
-            name="url"
-            id="url"
-            required
-            placeholder="https://example.com/article"
-          />
-        </div>
-        <div>
-          <label for="shortCode">Enter Short Code (optional)</label>
-          <input
-            type="text"
-            name="shortCode"
-            id="shortCode"
-            minlength="3"
-            maxlength="24"
-            pattern="[A-Za-z0-9-_]+"
-            placeholder="e.g. my-link"
-          />
-        </div>
-        <button type="submit" aria-label="Shorten URL">Shorten</button>
-      </form>`);
-});
-
 app.listen(PORT, () => {
     console.log(`Server started → http://localhost:${PORT}/`);
 });
