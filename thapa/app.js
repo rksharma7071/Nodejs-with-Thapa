0e43c61162d5bc0cd5559013eab37b9a2dc8a973
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-11-13 22:02:38 +0530

    Query Parameters in Express Real Examples & Practical Demo

--- thapa/app.js
+++ thapa/app.js
@@ -12,14 +12,30 @@ app.get("/", (req, res) => {
     // console.log("filename", import.meta.filename); // filename C:\Users\retes\Desktop\NodeJS\thapa\app.js
     // console.log("url: ", import.meta.url); // url:  file:///C:/Users/retes/Desktop/NodeJS/thapa/app.js
 
-    // const __filename = new URL(import.meta.url);
-    // const pathname = new URL(import.meta.url).pathname;
-    // console.log(`${__filename}\n ${pathname}`);
-
     const homePagePath = path.join(import.meta.dirname, "public", "index.html");
 
     res.sendFile(homePagePath);
 });
+
+app.get("/test", (req, res) => {
+    const __filename = new URL(import.meta.url).pathname;
+    const __dirname = path.dirname(__filename);
+    console.log({ __dirname, __filename });
+
+    res.send("This is a testing page!")
+})
+
+app.get("/profile/:username", (req, res) => {
+    res.send(`<h1>My username is ${req.params.username}!</h1>`)
+})
+
+app.get("/profile/:username/article/:slug", (req, res) => {
+    res.send(`<h1>My username is ${req.params.username}!</h1>
+        <h2>Blog: ${req.params.slug}</h2>
+        `)
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
