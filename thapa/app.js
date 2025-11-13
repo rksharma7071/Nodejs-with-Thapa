68246b3b063a55bc9ab88d6f22eabbf8505a33da
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-11-13 15:23:27 +0530

    es modules in nodejs caveats dirname filename fixes top level await explained

--- thapa/app.js
+++ thapa/app.js
@@ -4,6 +4,9 @@ import path from "path";
 const app = express();
 const PORT = 3000;
 
+const staticPath = path.join(import.meta.dirname, "public");
+app.use(express.static(staticPath));
+
 app.get("/", (req, res) => {
     // console.log("dirname", import.meta.dirname); // dirname C:\Users\retes\Desktop\NodeJS\thapa
     // console.log("filename", import.meta.filename); // filename C:\Users\retes\Desktop\NodeJS\thapa\app.js
@@ -13,7 +16,7 @@ app.get("/", (req, res) => {
     // const pathname = new URL(import.meta.url).pathname;
     // console.log(`${__filename}\n ${pathname}`);
 
-    const  homePagePath = path.join(import.meta.dirname, "public","index.html")   
+    const homePagePath = path.join(import.meta.dirname, "public", "index.html");
 
     res.sendFile(homePagePath);
 });
